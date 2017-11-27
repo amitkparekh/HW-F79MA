4ef012f91fb54ff431ebbcbf3808fd7a858b75d8
Amit Parekh <amit@amitkparekh.com>
2017-11-27 20:00:38 +0000

    Finish project 2

--- 02_Project/02_01.R
+++ 02_Project/02_01.R
@@ -1,10 +1,10 @@
-# the setup ---------------------------------------------------------------
-
+# sample data
 x <- c(
     0.324,
     0.177,
     0.163,
     0.317,
+    0.326,
     0.174,
     0.321,
     0.115,
@@ -12,47 +12,80 @@ x <- c(
     0.133
 )
 
-n <- 10
-a <- n
+n <- length(x)
 xMin <- 0.1
 
+# calculate beta
 b <- 0 
-for (i in 1:length(x)) {
+for (i in 1:n) {
     b <- b + log(x[i])
-}; b <- b - n*log(xMin)
+}
+b <- b - n*log(xMin)
 
-aGam <- factorial(a-1)
+# calculate alpha
+a <- n 
 
-# Q3  ---------------------------------------------------------------------
+trials <- 10000
 
-trials <- 1000
 
-dist <- 0
-for (y in 1:trials) {
-    dist[y] <- (b^a) / (aGam * y)
+# Plot posterior density
+
+postDensity <- 0
+for (i in 1:trials) {
+    postDensity[i] <- rgamma(1, a, b)
+}
+plot(density(postDensity),
+     main = "Fig 1: Distribution of the Posterior Density, for 10000 trials",
+     xlab="Theta",
+     ylab="Probability Density",
+     col="indianred1",
+     lwd=2)
+
+summary(postDensity)
+
+
+# Simulate posterior predictive density
+y <- 0
+for (i in 1:500) {
+    y[i] <- 0
+    
+    while (y[i] < 0.1) {
+        u <- runif(1, 0, 1)
+        theta <- rgamma(1, n, b)
+        y[i] <- xMin / ( (1-u)^(1/theta) )
+    }
 }
 
-plot(dist, log="x", type='l')
+
+plot(density(y), 
+    log="x", 
+    type='l',
+    main = "Fig 2: Posterior predictive density for the total claim amount for the next year",
+    ylab = "Posterior predictive density",
+    #xlab = "Total claim amount predicted for the next year",
+    col="deepskyblue2",
+    lwd=2)
+
+summary(y)
 
 
-# Q4 ----------------------------------------------------------------------
-
-trials2 <- 10000
+# Estimate probability that y exceeds £1m
 
 y <- 0
-for (i in 1:trials2) {
-    u <- runif(1, 0, 1)
-    theta <- rgamma(1, n, b)
-    y[i] <- xMin / ( (1-u)^(1/theta) )
+for (i in 1:10000) {
+    y[i] <- 0
+    
+    while (y[i] < 0.1) {
+        u <- runif(1, 0, 1)
+        theta <- rgamma(1, n, b)
+        y[i] <- xMin / ( (1-u)^(1/theta) )
+    }
 }
 
-probest <- (y > 1) %>% sum() / trials2
+probest <- (y > 1) %>% sum() / 10000
 
 
-# Q5 ----------------------------------------------------------------------
-
+# Calculate reserve value
 reserve <- quantile(y, 0.99)
-
-probRes <- (y > reserve) %>% sum() / trials2
-
+probRes <- (y > reserve) %>% sum() / trials
 
